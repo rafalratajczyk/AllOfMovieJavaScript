9cf62a65e1adba61e62ebeec9981f8a776fcde41
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-19 12:36:28 +0200

    change of function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-$(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
-        let searchText = $('#searchText').val();
+$(document).ready(function () {
+    $('#searchForm').on('submit', function (e) {
+        var searchText = $('#searchText').val();
         getMovies(searchText);
         e.preventDefault();
-        });
+    });
 });
 
 function getMovies(searchText) {
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=fa155f635119344d33fcb84fb807649b&query='+searchText)
-        .then((response) => {
-        console.log(response);
-        let movies = response.data.Search;
-        let output = '';
-        $.each(movies, (index, movie) => {
-            output += ` 
-                <div class="col-md-3">
-                    <div class="well text-center">
-                        <img src="${movie.Poster}">
-                        <h5>${movie.Title}</h5>
-                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                     </div>
-                </div>
-                `;
-        });
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=fa155f635119344d33fcb84fb807649b&query=' + searchText)
+        .then(function (response) {
+            var movies = response.data.results;
+            console.log(movies);
+            var output = '';
+            $.each(movies, function (index, movie) {
+                output += '<div class="col-md-3">';
+                output += '<div class="well text-center">';
+                output += '<img src="http://image.tmdb.org/t/p/w185/' + movie.poster_path + '">';
+                output += '<h5>' + movie.title + '</h5>';
+                output += '<a onclick=movieSelected("' + movie.id + ' class="btn btn-primary" href="#")>Movie Details</a>';
+                output += '</div>';
+                output += '</div>';
+            });
 
-        $('#movies').html(output);
-    })
+            $('#movies').html(output);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
 }
